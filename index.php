7af8a3b46e6df0a132f275598bde837d02a78728
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2019-06-15 17:46:13 +0530

    add php file

--- index.php
+++ index.php
@@ -1,4 +1,69 @@
 <?php require_once('inc/connection.php');?>
+<?php
+ 	//check for submission
+
+if(isset($_POST['submit'])){
+
+$error=array();
+
+//check if the username and password has been entered
+	if(!isset($_POST['email'])|| strlen(trim($_POST['email']))<1)
+	{
+		$error[]='Username is Missing/Invalid';
+	}
+
+	if(!isset($_POST['password'])|| strlen(trim($_POST['password']))<1)
+	{
+		$error[]='Password is Missing/Invalid';
+	}
+
+//check if there are any errors in the form
+
+	if (empty($errors)){
+
+ 
+
+      //save username and password into   variable
+
+	$email=mysqli_real_escape_string($connection,$_POST['email']);
+	$password=mysqli_real_escape_string($connection,$_POST['password']);
+	$hashed_password=$password;
+ 
+    //   prepare database query
+
+    $query="SELECT * FROM user WHERE email='{$email}'AND password ='{$hashed_password}' LIMIT 1";
+
+    $result_set=mysqli_query($connection,$query);
+
+     if($result_set){
+//query succesful
+     	if(mysqli_num_rows($result_set)==1)
+     	{
+     		//vallid user found
+	//redirect to users.php
+
+     			header('Location:users.php');
+     	}else{
+     		//user name and password invalid
+     		$errors[]='Invalid Username/Password';
+
+     	}
+
+     }else{
+     	$errors[]='Database query failed';
+     }
+
+	//check if the user is valid
+
+
+
+	
+	//if not , display  the error
+	}
+
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,8 +78,13 @@
 			
 			<fieldset>
 				<legend><h1>Log In</h1></legend>
+ 
+				<?php
+						if(isset ($errors) && !empty($errors)){
+							echo '<p class="error">Invalid Username/Password</p>';
+						}
 
-				<!--p class="error">Invalid Username/Password</p>-->
+				?>
 
 				<p>
 					<label for="">Username:</label>
---
+++ users.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<htm lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Users</title>
+</head>
+<body>
+	<h1>Users</h1>
+</body>
+</html>
